3c035109bde1cd7e446efaadc6d8022b6142db84
valerio capogna <capogna.valerio@gmail.com>
2017-05-05 15:57:42 +0200

    add images compression support

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ const autoprefixer = require('gulp-autoprefixer');
 const notify = require('gulp-notify');
 const plumber = require('gulp-plumber');
 const watch = require('gulp-watch');
+const image = require('gulp-image');
 const webpack = require('webpack-stream');
 const webpackUglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const browserSync = require('browser-sync').create();
@@ -17,6 +18,14 @@ function handleErrors(error) {
     message:  "<%= error.message %>",
   })(error);
   this.emit('end');
+}
+
+function execTaskAndReload(done) {
+  if (browserSync.active) {
+    browserSync.reload();
+  }
+
+  done();
 }
 
 var cssTask = function() {
@@ -85,16 +94,21 @@ var jsTask = function() {
 var serveTask = function() {
   browserSync.init(config.serve.browserSync);
   gulp.watch(config.serve.css, ['css']);
-  gulp.watch(config.serve.js, ['js-watch']);
+  gulp.watch(config.serve.js, ['watch-js']);
+  gulp.watch(config.serve.images, ['watch-images']);
   gulp.watch(config.serve.html).on('change', browserSync.reload);
+};
+
+var imagesTask = function() {
+  return gulp
+    .src(path.resolve(__dirname, config.images.input))
+    .pipe(image(config.images.options))
+    .pipe(gulp.dest(path.resolve(__dirname, config.images.output)));
 };
 
 gulp.task('css', cssTask);
 gulp.task('js', jsTask);
 gulp.task('serve', serveTask);
-gulp.task('js-watch', ['js'], function(done) {
-  if (browserSync.active) {
-    browserSync.reload();
-  }
-  done();
-});
+gulp.task('images', imagesTask);
+gulp.task('watch-js', ['js'], execTaskAndReload);
+gulp.task('watch-images', ['images'], execTaskAndReload);
